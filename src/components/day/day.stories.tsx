2435fa55e45e41d8bdc418fa13628cf4b436cfc0
zaitsev-av <zaitsev.av.12@gmail.com>
2024-04-06 13:07:24 +0300

    [develop]feat: updated history for the Day component

--- src/components/day/day.stories.tsx
+++ src/components/day/day.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { eachDayOfInterval } from 'date-fns';
 
 import { Day } from '@/components/day/day';
 
@@ -15,5 +16,25 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: { date: new Date(), displayMonth: new Date() }
+	args: { date: new Date(), displayMonth: new Date() },
+	render: function (args) {
+		const startDate = new Date();
+		const endDate = new Date(
+			startDate.getFullYear(),
+			startDate.getMonth(),
+			startDate.getDay() + 7
+		);
+
+		const dates = eachDayOfInterval({ start: startDate, end: endDate });
+		return (
+			<div style={{ display: 'flex', width: '500px', gap: 16 }}>
+				{dates.map(day => (
+					<Day
+						displayMonth={args.displayMonth}
+						date={day}
+					/>
+				))}
+			</div>
+		);
+	}
 };
